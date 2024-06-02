d8a9da02f896d7ec63e53014dd8fcb4500c86bf4
Prem Patel <pprem7548@gmail.com>
2024-06-02 17:29:31 +0530

    user side product details

--- src/main/java/UserCDIBeans/UserDisplayProductBean.java
+++ src/main/java/UserCDIBeans/UserDisplayProductBean.java
@@ -1,36 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
- */
 package UserCDIBeans;
 
 import Entitys.ProductTb;
 import RestFullClient.RestClient;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
+import loginBean.LoginBean;
 
-/**
- *
- * @author Admin
- */
 @Named(value = "userDisplayProductBean")
-@Dependent
-public class UserDisplayProductBean {
+@SessionScoped
+public class UserDisplayProductBean implements Serializable {
+    private RestClient rc;
+    private Collection<ProductTb> products;
+    private GenericType<Collection<ProductTb>> gp;
+    private Response res;
+    private GenericType<ProductTb> gpp;
+    private ProductTb p;
+    String size;
+    @Inject LoginBean lb;
+    Integer qty;
 
-    RestClient rc;
-    Collection<ProductTb> products;
-    GenericType<Collection<ProductTb>> gp;
-    Response res;
-    ProductTb p = new ProductTb();
-    
     public UserDisplayProductBean() {
         rc = new RestClient();
         products = new ArrayList<>();
-        gp = new GenericType<Collection<ProductTb>>(){};
+        gp = new GenericType<Collection<ProductTb>>() {};
+        gpp = new GenericType<ProductTb>() {};
+        p = new ProductTb();
     }
 
     public Collection<ProductTb> getProducts() {
@@ -50,6 +50,33 @@ public class UserDisplayProductBean {
     public void setP(ProductTb p) {
         this.p = p;
     }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public void getProductDetail(Integer pid) {
+        System.out.println("Fetching Product Details for ID: " + pid);
+        res = rc.getProductDetails(Response.class, String.valueOf(pid));
+        this.p = res.readEntity(gpp);
+        System.out.println("Product Name: " + p.getName());
+    }
     
-    
+    public String addToCart()
+    {
+        rc.addToCart(String.valueOf(lb.getuId()),String.valueOf(p.getId()),this.size,String.valueOf(this.qty), String.valueOf(p.getPrice()));
+        return "Products";
+    }
 }

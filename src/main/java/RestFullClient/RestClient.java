999702a0d8cb2d4f7411e65d5473f678dd196519
Prem Patel <pprem7548@gmail.com>
2024-06-12 11:11:28 +0530

    small corrections

--- src/main/java/RestFullClient/RestClient.java
+++ src/main/java/RestFullClient/RestClient.java
@@ -24,7 +24,7 @@ public class RestClient {
 
     private WebTarget webTarget;
     private Client client;
-    private static final String BASE_URI = "http://localhost:8080/Star_Glam_Attier/resources";
+    private static final String BASE_URI = "http://localhost:8080/SGA/resources";
 
     public RestClient() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
